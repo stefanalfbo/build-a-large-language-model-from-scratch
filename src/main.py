c3f31b61f5a868f0cc452811786e3cfcfe210b4c
Stefan Alfbo <stefan@alfbo.se>
2025-01-02 19:43:40 +0100

    Create a vocabulary function

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ def print_preview(file):
     with open(file, "r") as f:
         raw_text = f.read()
         preprocessed = basic_tokenizer(raw_text)
+        vocabulary = create_vocabulary(preprocessed)
 
         print(f"""Total number of characters: {len(raw_text):,}""")
         print("=========================================================")
@@ -12,6 +13,10 @@ def print_preview(file):
         print(f"""Total number of tokens: {len(preprocessed):,}""")
         print("=========================================================")
         print(f"""Preview: {preprocessed[:30]}""")
+        print()
+        print(f"""Vocabulary size: {len(vocabulary):,}""")
+        print("=========================================================")
+        print(f"""Preview: {list(vocabulary.items())[:10]}""")
 
 def basic_tokenizer(text: str) -> list[str]:
     """Simple tokenizer that splits text into tokens. 
@@ -25,6 +30,11 @@ def basic_tokenizer(text: str) -> list[str]:
     result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 
     return [token for token in result if token.strip()]
+
+def create_vocabulary(preprocessed: list[str]) -> list[str]:
+    all_tokens = sorted(set(preprocessed))
+
+    return {token: i for i, token in enumerate(all_tokens)}
 
 def main():
     print_preview("data/the-verdict.txt")
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,4 @@
-from src.main import basic_tokenizer
+from src.main import basic_tokenizer, create_vocabulary
 
 def test_basic_tokenizer():
     # Arrange
@@ -10,3 +10,16 @@ def test_basic_tokenizer():
 
     # Assert
     assert preprocessed == expected
+
+def test_create_vocabulary():
+    # Arrange
+    preprocessed = ["Hello", ",", "world", ".", "Is", "this", "--", "a", "test", "?"]
+    expected = {
+        ",": 0, "--": 1, ".": 2, "?": 3, "Hello": 4, "Is": 5, "a": 6, "test": 7, "this": 8, "world": 9
+    }
+
+    # Act
+    vocabulary = create_vocabulary(preprocessed)
+
+    # Assert
+    assert vocabulary == expected
