ec85826e69ffd5b4360b4f838d9aa6d5c23605ab
Stefan Alfbo <stefan@alfbo.se>
2025-01-05 13:29:04 +0100

    Extend with more tests for the create dataloader function to show case different strides/max_length

--- tests/data_loader_test.py
+++ tests/data_loader_test.py
@@ -20,3 +20,47 @@ def test_create_dataloader_v1():
     # Assert
     assert torch.equal(first_batch[0], expected_input_token_ids)
     assert torch.equal(first_batch[1], expected_target_token_ids)
+
+
+def test_create_dataloader_with_max_length_2_stride_2():
+    # Arrange
+    expected_input_token_ids = torch.tensor(
+        [[40, 367], [2885, 1464], [1807, 3619], [402, 271]]
+    )
+    with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=4, max_length=2, stride=2, shuffle=False
+    )
+
+    # Act
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+
+    # Assert
+    assert torch.equal(first_batch[0], expected_input_token_ids)
+
+
+def test_create_dataloader_with_max_length_8_stride_2():
+    # Arrange
+    expected_input_token_ids = torch.tensor(
+        [
+            [40, 367, 2885, 1464, 1807, 3619, 402, 271],
+            [2885, 1464, 1807, 3619, 402, 271, 10899, 2138],
+            [1807, 3619, 402, 271, 10899, 2138, 257, 7026],
+            [402, 271, 10899, 2138, 257, 7026, 15632, 438],
+        ]
+    )
+
+    with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=4, max_length=8, stride=2, shuffle=False
+    )
+
+    # Act
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+
+    # Assert
+    assert torch.equal(first_batch[0], expected_input_token_ids)
